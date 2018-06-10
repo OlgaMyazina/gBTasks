1fe8a58086462527974d7e6b578daca12231bbc6
Olga <olga_kuznetsova@hotmail.com>
2018-06-10 22:07:27 +0300

    Рефакторинг

--- task3.js
+++ task3.js
@@ -1,13 +1,4 @@
-/*Сложение временных промежутков
-Сложить 2 временных промежутка и вывести общую длительность в человекочитаемом виде.
-В выводе использовать часы, минуты и секунды (при необходимости).
-На вход подается два числа t1 и t2, разделенные пробелом. 1 <= t1, t2 <= 100000
-
-Пример
-> node task.js 1 1
-> 2 секунды
-> node task.js 3600 3599
-> 1 час 59 минут 59 секунд*/
+/* Task 3*/
 
 function formatSum(t1, t2) {
   const sum = +t1 + +t2;
@@ -16,43 +7,28 @@ function formatSum(t1, t2) {
   const sec = sum - hour * 3600 - min * 60;
 
   if (hour === 0) {
-    if (min === 0) return `${sec} ${countSec(sec)}`;
-    return `${min} ${countMin(min)} ${sec} ${countSec(sec)}`;
+    if (min === 0) return `${formatSec(sec)}`;
+    return `${formatMin(min)} ${formatSec(sec)}`;
   }
-  return `${hour} ${countHour(hour)} ${min} ${countMin(min)} ${sec} ${countSec(
-    sec
-  )}`;
+  return `${formatHour(hour)} ${formatMin(min)} ${formatSec(sec)}`;
 }
 
-function countHour(count) {
-  if (count % 100 > 4 && count % 100 < 20) return 'часов';
+function textTime(count, first, second, fifty) {
+  if (count % 100 > 4 && count % 100 < 20) return fifty;
   else if (count % 10 < 5) {
-    const names = ['часов', 'час', 'часа', 'часа', 'часа', 'часов'];
+    const names = [fifty, first, second, second, second, fifty];
     return names[count % 10];
-  } else return 'часов';
+  } else return fifty;
 }
 
-function countMin(count) {
-  if (count % 100 > 4 && count % 100 < 20) return 'минут';
-  else if (count % 10 < 5) {
-    const names = ['минут', 'минута', 'минуты', 'минуты', 'минуты', 'минут'];
-    return names[count % 10];
-  } else return 'минут';
+function formatSec(sec) {
+  return `${sec} ${textTime(sec, 'секунда', 'секунды', 'секунд')}`;
 }
-
-function countSec(count) {
-  if (count % 100 > 4 && count % 100 < 20) return 'секунд';
-  else if (count % 10 < 5) {
-    const names = [
-      'секунд',
-      'секунда',
-      'секунды',
-      'секунды',
-      'секунды',
-      'секунд'
-    ];
-    return names[count % 10];
-  } else return 'секунд';
+function formatMin(min) {
+  return `${min} ${textTime(min, 'минута', 'минуты', 'минут')}`;
+}
+function formatHour(hour) {
+  return `${hour} ${textTime(hour, 'час', 'часа', 'часов')}`;
 }
 
 const t1 = process.argv[2];
