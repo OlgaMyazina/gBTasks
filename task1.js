b6f158aabb266ffd5aed90ec7462dbe257b3b499
Olga <olga_kuznetsova@hotmail.com>
2018-06-10 13:12:37 +0300

    Замена console.log на process.stdout.write Замена комментария с условием задачи

--- task1.js
+++ task1.js
@@ -1,11 +1,5 @@
-/*1. Фильтр блокировок
-Реализовать функцию, исключающую из входного строки русские буквы Р, К и Н в любом регистре.
-На вход подается строка длины n. 1 <= n <= 10000
-
-Пример
-> node task.js 'Окраина'
-> Оаиа */
+/* Task 1 */
 
 const strIn = process.argv[2];
 const re = /\[Р|Н|К|р|н|к]/gi;
-console.log(strIn.replace(re, ''));
+process.stdout.write(strIn.replace(re, ''));
