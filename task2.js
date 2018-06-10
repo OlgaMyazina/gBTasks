a27c7ce34479cfa2efa43077e4fd622e338429ec
Olga <olga_kuznetsova@hotmail.com>
2018-06-10 22:19:05 +0300

    Рефакторинг - вывод, текст условия задания

--- task2.js
+++ task2.js
@@ -1,29 +1,22 @@
-/*2. Минимальный сдвиг
-Найти минимальное количество сдвигов массива a, после которых он будет равен массиву b.
-Сдвиг можно осуществлять в любую сторону.
-Вывести -1, если такой сдвиг не существует.
-На вход подается две строки длины a и b. 1 <= a, b <= 10000
+/* Task 2*/
 
-Пример
-> node task.js '1234' '2341'
-> 1 */
 function shift(strA, strB) {
   if (strA.length < strB.length) return -1;
   let count = strA.indexOf(strB);
   if (count !== -1) return count;
   for (let i = 0; i < strA.length; ++i) {
-    strA = shiftString(strA);
+    strA = shiftStr(strA);
     count = strA.indexOf(strB);
     if (count !== -1) return count + i + 1;
   }
   return -1;
 }
 
-function shiftString(str) {
+function shiftStr(str) {
   str = str.slice(1) + str.slice(0, 1);
   return str;
 }
 
 const strA = process.argv[2];
 const strB = process.argv[3];
-console.log(shift(strA, strB));
+process.stdout.write(shift(strA, strB).toString());
