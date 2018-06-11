a0cf4db12e8ad6e797e1ba93b1766fe846b6a69f
Olga <olga_kuznetsova@hotmail.com>
2018-06-11 17:07:56 +0300

    рефкторинг

--- task3.js
+++ task3.js
@@ -1,37 +1,39 @@
 /* Task 3*/
 
 function formatSum(t1, t2) {
-  const sum = +t1 + +t2;
+  const sum = t1 + t2;
   const hour = Math.floor(sum / 3600);
   const min = Math.floor((sum - hour * 3600) / 60);
   const sec = sum - hour * 3600 - min * 60;
 
-  if (hour === 0) {
-    if (min === 0) return `${formatSec(sec)}`;
-    return `${formatMin(min)} ${formatSec(sec)}`;
-  }
-  return `${formatHour(hour)} ${formatMin(min)} ${formatSec(sec)}`;
+  const result = `${hour === 0 ? '' : formatHour(hour)} ${
+    min === 0 ? '' : formatMin(min)
+  }${sec === 0 ? '' : formatSec(sec)}`;
+  return result.trim();
 }
 
-function textTime(count, first, second, fifty) {
-  if (count % 100 > 4 && count % 100 < 20) return fifty;
+function textTime(count, first, second, fifth) {
+  if (count % 100 > 4 && count % 100 < 20) return fifth;
   else if (count % 10 < 5) {
-    const names = [fifty, first, second, second, second, fifty];
+    const names = [fifth, first, second, second, second];
     return names[count % 10];
-  } else return fifty;
+  } else return fifth;
 }
 
 function formatSec(sec) {
-  return `${sec} ${textTime(sec, 'секунда', 'секунды', 'секунд')}`;
+  return `${sec} ${textTime(sec, ...wordSec)}`;
 }
 function formatMin(min) {
-  return `${min} ${textTime(min, 'минута', 'минуты', 'минут')}`;
+  return `${min} ${textTime(min, ...wordMin)} `;
 }
 function formatHour(hour) {
-  return `${hour} ${textTime(hour, 'час', 'часа', 'часов')}`;
+  return `${hour} ${textTime(hour, ...wordHour)}`;
 }
 
-const t1 = process.argv[2];
-const t2 = process.argv[3];
+const t1 = +process.argv[2];
+const t2 = +process.argv[3];
+const wordHour = ['час', 'часа', 'часов'];
+const wordMin = ['минута', 'минуты', 'минут'];
+const wordSec = ['секунда', 'секунды', 'секунд'];
 
 process.stdout.write(formatSum(t1, t2));
